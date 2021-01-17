b6df842be6a8e78bb23681308bef64c5a2c48ed8
pazsitni.imre <imre.pazsitni@gmail.com>
2021-01-17 15:08:07 +0100

    Added Babel, Removed unnecessary generated lines, Simple landing page stylized
    Babel for ES6 backwards compatibility
    Body stylized for landing page
    Components folder added.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+ 
+
+  render() { 
+    return <div></div>
+   }
+
+    
+}
+ 
+export default App;
